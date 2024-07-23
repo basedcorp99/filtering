0bba49d76c4701c80528f3f920403c974348061f
John Doe <john@doe.com>
2024-07-23 15:51:11 +0200

    start of safe/money

--- sql/create_entries.sql
+++ sql/create_entries.sql
@@ -6,3 +6,14 @@ CREATE TABLE redirects (
     ttclid BOOLEAN DEFAULT FALSE,
     last_access TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE entries_new (
+    id SERIAL PRIMARY KEY,
+    stream_name VARCHAR(255) NOT NULL,
+    safe_link VARCHAR(255),
+    money_link VARCHAR(255),
+    money_active BOOLEAN DEFAULT FALSE,
+    utm BOOLEAN DEFAULT FALSE,
+    ttclid BOOLEAN DEFAULT FALSE,
+    last_access TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
